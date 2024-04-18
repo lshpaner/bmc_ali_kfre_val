bfcbde926f7802269f86c8924d6de48096fd8c02
lshpaner <lshpaner@ucla.edu>
2024-04-18 11:34:27 -0700

    preprocessed df, also created one for eda, saved to path

--- python_scripts/functions.py
+++ python_scripts/functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random  # for generating random numbers and performing random operations
 import os
 import sys
 
@@ -15,3 +16,37 @@ def ensure_directory(path):
     else:
         print(f"Directory exists: {path}")
 
+
+################################################################################
+######################## Generate Random Patient IDs ###########################
+################################################################################
+
+def add_patient_ids(df, seed=None):
+    """
+    Add a column of unique, 9-digit patient IDs to the dataframe.
+
+    This function sets a random seed and then generates a 9-digit patient ID for 
+    each row in the dataframe. The new IDs are added as a new 'Patient_ID' 
+    column, which is placed as the first column in the dataframe.
+
+    Args:
+        df (pd.DataFrame): The dataframe to add patient IDs to.
+        seed (int, optional): The seed for the random number generator. 
+        Defaults to 222.
+
+    Returns:
+        pd.DataFrame: The updated dataframe with the new 'Patient_ID' column.
+    """
+    random.seed(seed)
+
+    # Generate a list of unique IDs
+    patient_ids = [''.join(random.choices('0123456789', k=9)) 
+                   for _ in range(len(df))]
+
+    # Create a new column in df for these IDs
+    df['Patient_ID'] = patient_ids
+
+    # Make 'Patient_ID' the first column and set it to index
+    df = df.set_index('Patient_ID')
+
+    return df
