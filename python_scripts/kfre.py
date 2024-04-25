ed077dc22278419121e88ed48b511091440bfc82
lshpaner <lshpaner@ucla.edu>
2024-04-24 17:30:56 -0700

    fixed unit metric for calcium from mg/g to mg/dL

--- python_scripts/kfre.py
+++ python_scripts/kfre.py
@@ -40,12 +40,6 @@ class RiskPredictor:
         """
         self.data = data
         self.columns = columns
-        # if apply_conversions:
-        #     # Perform the unit conversions with flexible column names
-        #     self.data["uPCR (mg/g)"] = self.data[self.columns['uPCR_mmol']] * 4
-        #     self.data["Calcium (mg/g)"] = self.data[self.columns['calcium_mmol']] * 4
-        #     self.data["Phosphate (mg/g)"] = self.data[self.columns['phosphate_mmol']] * 3.1
-        #     self.data["Albumin (g/dL)"] = self.data[self.columns['albumin_g_per_l']] / 10
 
     def perform_conversions(self):
         """
@@ -58,7 +52,7 @@ class RiskPredictor:
         - Albumin from g/L to g/dL
         """
         self.data["uPCR (mg/g)"] = self.data[self.columns["uPCR_mmol"]] * 8.84
-        self.data["Calcium (mg/g)"] = self.data[self.columns["calcium_mmol"]] * 4
+        self.data["Calcium (mg/dL)"] = self.data[self.columns["calcium_mmol"]] * 4
         self.data["Phosphate (mg/g)"] = self.data[self.columns["phosphate_mmol"]] * 3.1
         self.data["Albumin (g/dL)"] = self.data[self.columns["albumin_g_per_l"]] / 10
 
@@ -250,8 +244,9 @@ def risk_pred(
     # Check if the region is North American
     is_north_american = Region == "North American"
     # Set alpha based on region and years
+
     alpha = np.where(
-        Region == "North American",
+        is_north_american,
         np.where(years == 2, alpha_values[(True, 2)], alpha_values[(True, 5)]),
         np.where(years == 2, alpha_values[(False, 2)], alpha_values[(False, 5)]),
     )
